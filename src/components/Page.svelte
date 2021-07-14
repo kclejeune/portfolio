505c9e0669ff36991c638eebc055521f69f0c4bf
Kennan LeJeune <kennan@case.edu>
2021-07-13 20:33:58 -0400

    don't allow custom css on pages

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -3,15 +3,11 @@
 
     export let id: string;
     export let title: string;
-    export let customClass = "";
     export let lightBackground: string;
     export let darkBackground: string;
 </script>
 
-<div
-    {id}
-    class="w-screen min-h-screen {lightBackground} dark:{darkBackground} {customClass}"
->
+<div {id} class="w-screen min-h-screen {lightBackground} dark:{darkBackground}">
     <div class="py-4">
         <Header>{title}</Header>
     </div>
