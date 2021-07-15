def4f214c188511b40c8a8164d61f67ba31e706e
Kennan LeJeune <kennan@case.edu>
2021-07-15 15:24:47 -0400

    run class sorter

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a
-    class="mb-2 font-light text-primary-800 underline text-md sm:hover:text-primary-600 dark:text-primary-300 sm:dark:hover:text-primary-400"
+    class="mb-2 font-light underline text-primary-800 text-md sm:hover:text-primary-600 dark:text-primary-300 sm:dark:hover:text-primary-400"
     href={url}
 >
     <slot />
--- src/components/LoadingCard.svelte
+++ src/components/LoadingCard.svelte
@@ -4,11 +4,11 @@
 
 <Card title="Loading...">
     <div class="grid grid-cols-3 gap-4 mt-2">
-        <div class="h-8 bg-neutral-200 rounded dark:bg-neutral-600 animate-pulse" />
-        <div class="h-8 bg-neutral-200 rounded dark:bg-neutral-600 animate-pulse" />
-        <div class="h-8 bg-neutral-200 rounded dark:bg-neutral-600 animate-pulse" />
-        <div class="h-8 col-span-2 bg-neutral-200 rounded dark:bg-neutral-600 animate-pulse" />
-        <div class="h-8 bg-neutral-200 rounded dark:bg-neutral-600 animate-pulse" />
+        <div class="h-8 rounded bg-neutral-200 dark:bg-neutral-600 animate-pulse" />
+        <div class="h-8 rounded bg-neutral-200 dark:bg-neutral-600 animate-pulse" />
+        <div class="h-8 rounded bg-neutral-200 dark:bg-neutral-600 animate-pulse" />
+        <div class="h-8 col-span-2 rounded bg-neutral-200 dark:bg-neutral-600 animate-pulse" />
+        <div class="h-8 rounded bg-neutral-200 dark:bg-neutral-600 animate-pulse" />
         <div class="..." />
         <div class="col-span-2 ..." />
     </div>
