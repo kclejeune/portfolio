f3a778565d747a6c954f7ff061ad736480b044ea
Kennan LeJeune <kennan@case.edu>
2021-08-13 14:29:32 -0400

    remove redundant tailwind jit package

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,29 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess({
-		"postcss": true
-	})],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-			env: {
-				host: 'HOST',
-				port: 'PORT'
-			}
-		})
-	}
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte",
+        adapter: adapter({
+            // default options are shown
+            out: "build",
+            precompress: false,
+            env: {
+                host: "HOST",
+                port: "PORT",
+            },
+        }),
+    },
 };
 
 export default config;
-
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,18 @@
 const colors = require("tailwindcss/colors");
 
 const config = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
-	darkMode: "media",
-	theme: {
-		extend: {},
-		colors: {
-			...colors,
-			primary: colors.blue,
-			neutral: colors.coolGray,
-		},
-	},
-	plugins: [],
+    mode: "jit",
+    purge: ["./src/**/*.{html,js,svelte,ts}"],
+    darkMode: "media",
+    theme: {
+        extend: {},
+        colors: {
+            ...colors,
+            primary: colors.blue,
+            neutral: colors.coolGray,
+        },
+    },
+    plugins: [],
 };
 
 module.exports = config;
