1426afaf366ac02a294569bd333573209047ea99
Kennan LeJeune <kennan@case.edu>
2021-08-16 20:55:20 -0400

    update icons to use inline svg, remove extra assets

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     export let link: string;
-    export let iconClass: string;
-    export let customClass: string = "";
     export let name: string;
-    export let colorClass = "text-primary-300 sm:hover:text-primary-500";
     export let newTab: boolean = true;
 </script>
 
@@ -16,10 +13,10 @@
         href={link}
         on:click
     >
-        <i class="{colorClass} {iconClass} {customClass}" />
+        <slot />
     </a>
 {:else}
     <a on:click alt={name} aria-label={name} href={link}>
-        <i class="{colorClass} {iconClass} {customClass}" />
+        <slot />
     </a>
 {/if}
