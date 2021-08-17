6292318c7b38c4c78c72289c8aa881ddf3ae8b9f
Kennan LeJeune <kennan@case.edu>
2021-08-16 20:59:09 -0400

    update sveltekit, adapters, svelte core, and tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -14,15 +14,7 @@ const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: "#svelte",
-        adapter: adapter({
-            // default options are shown
-            out: "build",
-            precompress: false,
-            env: {
-                host: "HOST",
-                port: "PORT",
-            },
-        }),
+        adapter: adapter(),
     },
 };
 
