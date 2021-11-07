0be26bcedd9f2afb898b824af4f82ef20b837b7f
Kennan LeJeune <kennan@case.edu>
2021-11-07 01:41:52 -0500

    update about section text

--- src/lib/pages/About.svelte
+++ src/lib/pages/About.svelte
@@ -16,28 +16,27 @@
       and I've been addicted ever since. Outside of my studies, I'm an avid
       cyclist, musician, linux enthusiast. Prior to college, I was an active
       competitor in the Rubik's Cube speedsolving community. During the years
-      when I was actively competing, my highlights included a North American
-      Record, 90 top 3 finishes (35 gold, 32 silver, 23 bronze), and a top rank
-      of 12th in the world for the 3x3 Rubik's Cube.
+      when I was actively competing,
+      <Link url="https://www.worldcubeassociation.org/persons/2013LEJE03">
+        my highlights
+      </Link> included a North American Record, 90 podium finishes, and a top global
+      rank of of 7th and 12th for the 2x2 and 3x3 Rubik's Cubes, respectively.
     </p>
     <br />
     <p>
-      Currently, I work on graduate research in the <Link
+      Currently, I work on graduate machine learning research in the <Link
         url="https://engineering.case.edu/computer-and-data-sciences/academics/computer-science/bachelor-science"
       >
         CWRU Department of Computer and Data Sciences
       </Link>
-      under Dr. Soumya Ray. My work investigates knowledge propogation through networks
-      in multi-agent learning systems, and aims to develop dynamic optimization methods
-      to improve overall network performance with limited resource allocation.
-    </p>
-    <br />
-    <p>
-      After graduation, I'm transitioning to full time work on Software
-      Engineering and Data Science at the
+      under Dr. Soumya Ray. My thesis is focused on accelerated training in communicative
+      learning networks using dynamic structure adaptation. After graduation, I'm
+      transitioning to full time work on Software Engineering and Data Science at
+      the
       <Link url="https://www.jhuapl.edu/">
         Johns Hopkins University Applied Physics Laboratory</Link
-      >, where I do work on automated biological threat assessment and full stack software engineering with Angular and Java Spring.
+      >, where I work on automated systems for biological threat assessment and
+      full stack software engineering with Angular, Kotlin, and Java Spring.
     </p>
   </Text>
 </Page>
