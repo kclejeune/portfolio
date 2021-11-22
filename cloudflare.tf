2abd0669f89578ecfa0654612ba7f095d8d7818e
Kennan LeJeune <kennan@case.edu>
2021-11-22 04:01:46 -0500

    add terraform config for cloudflare resources

---
+++ cloudflare.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kclejeune"
+    workspaces { name = "portfolio" }
+  }
+}
+
+provider "cloudflare" {}
+
+locals {
+  deploy_target = "portfolio-kte.pages.dev"
+  canonical     = "kclj.io"
+  domains       = ["kclj.io", "kennanlejeune.com", "kclejeune.com"]
+}
+
+resource "cloudflare_zone" "zones" {
+  for_each = toset(local.domains)
+  zone     = each.key
+  plan     = "free"
+}
+
+resource "cloudflare_record" "root_record" {
+  for_each = toset(local.domains)
+  name     = "@"
+  zone_id  = cloudflare_zone.zones[each.key].id
+  type     = "CNAME"
+  value    = local.deploy_target
+  proxied  = true
+}
+
+resource "cloudflare_record" "www_record" {
+  for_each = toset(local.domains)
+  zone_id  = cloudflare_zone.zones[each.key].id
+  name     = "www"
+  type     = "CNAME"
+  value    = local.deploy_target
+  proxied  = true
+}
+
+resource "cloudflare_page_rule" "redirect_root" {
+  priority = 1
+  for_each = setsubtract(local.domains, [local.canonical])
+  target   = "https://${each.key}/*"
+  zone_id  = cloudflare_zone.zones[each.key].id
+
+  actions {
+    forwarding_url {
+      url         = "https://${local.canonical}/$1"
+      status_code = 302
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "redirect_www" {
+  priority = 1
+  for_each = toset(local.domains)
+  target   = "https://www.${each.key}/*"
+  zone_id  = cloudflare_zone.zones[each.key].id
+  actions {
+    forwarding_url {
+      url         = "https://${local.canonical}/$1"
+      status_code = 302
+    }
+  }
+}
+
+resource "cloudflare_zone_settings_override" "settings" {
+  for_each = toset(local.domains)
+  zone_id  = cloudflare_zone.zones[each.key].id
+  settings {
+    ssl           = "flexible"
+    rocket_loader = "on"
+    minify {
+      html = "on"
+      css  = "on"
+      js   = "on"
+    }
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+    email_obfuscation        = "on"
+  }
+}
